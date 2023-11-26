a387d3093ec116cc1532aeffd17dc6be24059ae0
soyebsuvo <soyebbeen@gmail.com>
2023-11-26 20:41:09 +0600

    navbar added

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
 
 export default function MainLayouts() {
-  return (
-    <div>
-      <Outlet></Outlet>
-    </div>
-  )
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    )
 }
