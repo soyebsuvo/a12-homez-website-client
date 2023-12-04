907a821e78d8b93a1a7dd20848be8e22086a444c
soyebsuvo <soyebbeen@gmail.com>
2023-12-04 23:44:06 +0600

    search functionality added in all properties page

--- src/Pages/AllProperties/AllProperties.jsx
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -1,26 +1,28 @@
 import FirstSlide from "../Home/HomeComponents/Banner/FirstSlide";
 import PropertyCard from "./PropertyCard";
 import image from '../../assets/home-5-2.jpg';
-import Loader from "../../Components/Loader";
+// import Loader from "../../Components/Loader";
 import { Helmet } from "react-helmet-async";
-import useVerifiedProperties from "../../Hooks/useVerifiedProperties";
-import { useState } from "react";
+// import useVerifiedProperties from "../../Hooks/useVerifiedProperties";
+import { useEffect, useState } from "react";
+import { GrFormSearch } from 'react-icons/gr';
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+// import { useQuery } from "@tanstack/react-query";
 
 export default function AllProperties() {
-    const [ sort , setSort ] = useState(false)
-    // const [allProperties, setAllProperties] = useState([]);
-    // useEffect(() => {
-    //     fetch('https://a-12-homez-server.vercel.app/properties')
-    //         .then(res => res.json())
-    //         .then(data => setAllProperties(data))
-    // }, [])
-    const [ allVerifiedProperties , isAllVerifiedPropertiesPending ] = useVerifiedProperties();
+    const [sort, setSort] = useState(false)
+    const [search , setSearch] = useState('')
+    const [allVerifiedProperties , setAllVerifiedProperties ] = useState([]) 
+    const axiosSecure = useAxiosSecure();
 
-    if (isAllVerifiedPropertiesPending) {
-        return <Loader></Loader>
-    }
-    const sortedLowToHigh = [...allVerifiedProperties].sort((a,b) => parseInt(a?.price?.min) - parseInt(b?.price?.min));
-    const sortedHighToLow = [...allVerifiedProperties].sort((x,y) => parseInt(y?.price?.min) - parseInt(x?.price?.min));
+    useEffect(() => {
+        axiosSecure.get(`/propertiesbysearch?search=${search}`)
+        .then(res => {
+            setAllVerifiedProperties(res.data)
+        })
+    } , [axiosSecure , search])
+    const sortedLowToHigh = [...allVerifiedProperties].sort((a, b) => parseInt(a?.price?.min) - parseInt(b?.price?.min));
+    const sortedHighToLow = [...allVerifiedProperties].sort((x, y) => parseInt(y?.price?.min) - parseInt(x?.price?.min));
     return (
         <div>
             <Helmet>
@@ -30,13 +32,19 @@ export default function AllProperties() {
             <div className="mt-5">
                 <h2 className="text-center text-4xl font-bold w-96 mx-auto border-b-2 pb-3">All Properties</h2>
             </div>
-            <div className="md:px-20 py-6">
-                <p className="text-sm text-gray-500 py-2">Price</p>
-                <button className="px-3 py-1 rounded-full text-white font-bold bg-[#EB6753]" onClick={() => setSort(!sort)}>{sort ? 'See High to Low' : 'See Low to High'}</button>
+            <div className="md:px-20 py-6 flex justify-between items-center">
+                <div>
+                    <p className="text-sm text-gray-500 py-2">Price</p>
+                    <button className="px-3 py-1 rounded-full text-white font-bold bg-[#EB6753]" onClick={() => setSort(!sort)}>{sort ? 'See High to Low' : 'See Low to High'}</button>
+                </div>
+                <div className="flex justify-end items-center">
+                    <input onChange={(e) => setSearch(e.target.value)} className="px-2 py-2 w-56 border-2 border-gray-500 rounded-lg" type="text" name="item" id="item" placeholder="Search Food Name..." />
+                    <span className="absolute"><GrFormSearch className="text-black text-3xl"></GrFormSearch></span>
+                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6 pb-12 px-20">
                 {
-                  sort ? sortedLowToHigh?.map(item => <PropertyCard key={item._id} item={item}></PropertyCard>) : sortedHighToLow?.map(item => <PropertyCard key={item._id} item={item}></PropertyCard>) 
+                    sort ? sortedLowToHigh?.map(item => <PropertyCard key={item._id} item={item}></PropertyCard>) : sortedHighToLow?.map(item => <PropertyCard key={item._id} item={item}></PropertyCard>)
                 }
             </div>
 
